6e4b7744819ce2ba54f3c5a35304e5f5d3220596
GondowTakashi <dokyogames@dokyougemusu-no-MacBook-Pro-4.local>
2014-09-03 18:49:54 +0900

    自機の残機と敵のHPの表示

---
+++ Assets/script/Enemy_hp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_hp : MonoBehaviour {
+	public int cnt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cnt>2) Destroy(this.gameObject);
+		cnt++;
+	}
+}
