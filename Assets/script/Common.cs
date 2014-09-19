5897ea9fafd5877421c0d614ba8780f8228e8c6f
GondowTakashi <dokyogames@dokyougemusu-no-MacBook-Pro-4.local>
2014-09-19 18:12:04 +0900

    難易度選択の追加

--- Assets/script/Common.cs
+++ Assets/script/Common.cs
@@ -1,7 +1,11 @@
-﻿namespace Common
+﻿using UnityEngine;
+namespace Common
 {
-	class Constant
-	{
-	public const float PI = 3.141592f;
+	class Constant{
+		public const float PI = 3.141592f;
+		public static float window_rate = Screen.height / 400.0f;
+	}
+	enum Enemy{
+		normal,boss
 	}
 }
---
+++ Assets/script/FontResize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FontResize : MonoBehaviour {
+	//デフォルト縦サイズ
+	float d_height = 400.0f;
+
+	//最初に固定
+	void Start () {
+		//現在の縦サイズ
+		float s_height = Screen.height;
+		//比率・大きい方に合わせる
+		float resize_rate = s_height / d_height;
+		guiText.fontSize =(int)( guiText.fontSize * resize_rate );  
+	}
+	// Update is called once per frame
+	void Update () {}
+}
